8df1cec8eff33723231a0793032ac304f2560584
ruona <ogheneruonaagadagba4@gmail.com>
2021-08-03 22:45:54 +0100

    started the natour project

---
+++ src/components/body.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderComponent from "./header.component";
+import "../styles/body.styles.css";
+
+const Body = () => {
+  return (
+    <div>
+      <HeaderComponent />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/header.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/header.styles.css";
+import Logo from "../images/logo-white.png";
+
+const HeaderComponent = () => {
+  return (
+    <header className="header">
+      <div className="logo-box">
+        <img src={Logo} alt="Logo" className="logo" />
+      </div>
+
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">Outdoors</span>
+          <span className="heading-primary-sub">is where life happens</span>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderComponent;
