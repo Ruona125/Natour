a9603c15cf00b6064aed4923641c6652232996aa
ruona <ogheneruonaagadagba4@gmail.com>
2021-08-05 12:36:05 +0100

    modified the styles

--- src/components/header.component.jsx
+++ src/components/header.component.jsx
@@ -5,17 +5,17 @@ import Logo from "../images/logo-white.png";
 const HeaderComponent = () => {
   return (
     <header className="header">
-      <div className="logo-box">
-        <img src={Logo} alt="Logo" className="logo" />
+      <div className="header__logo-box">
+        <img src={Logo} alt="Logo" className="header__logo" />
       </div>
 
-      <div className="text-box">
+      <div className="header__text-box">
         <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
+          <span className="heading-primary--main">Outdoors</span>
+          <span className="heading-primary--sub">is where life happens</span>
         </h1>
 
-        <a href="#" className="btn btn-white btn-animated">
+        <a href="#" className="btn btn--white btn--animated">
           Discover our tours
         </a>
       </div>
