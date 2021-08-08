d7f7f08d8bc0baed31dd5418cd9f8e795b22c19a
ruona <ogheneruonaagadagba4@gmail.com>
2021-08-08 22:05:08 +0100

    finished the section features component

--- src/components/body.component.jsx
+++ src/components/body.component.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import HeaderComponent from "./header.component";
 import "../styles/body.styles.scss";
 import { MainSection } from "./mainSection.component";
-
+import { SectionFeatures } from "./sectionFeatures.component";
 const Body = () => {
   return (
     <div>
       <HeaderComponent />
       <MainSection />
+      <SectionFeatures />
     </div>
   );
 };
